1492ddacb274d0d9ad5ad9b41e9066c4e9c844c7
Jose Carrillo <soyjrcarrillo@gmail.com>
2025-08-14 15:36:15 -0500

    feat(http): enhance HTTP method handling in API Gateway event

--- src/index.ts
+++ src/index.ts
@@ -5,9 +5,25 @@ import { HttpStatus, HttpStatusMessage } from '@/constants';
 export const handler = async (event: APIGatewayProxyEventV2, _context: Context): Promise<APIGatewayProxyResult> => {
 	const method = getHttpEventMethod(event);
 
-	return createResponse(HttpStatus.OK, HttpStatusMessage[HttpStatus.OK], {
-		message: HttpStatusMessage[HttpStatus.OK],
-		method,
-		context: _context,
-	});
+	switch (method) {
+		case 'GET':
+			return createResponse(HttpStatus.OK, HttpStatusMessage[HttpStatus.OK], {
+				message: 'GET method received',
+				method,
+				context: _context,
+				body: event,
+			});
+		case 'POST':
+			return createResponse(HttpStatus.OK, HttpStatusMessage[HttpStatus.OK], {
+				message: 'POST method received',
+				method,
+				context: _context,
+			});
+		default:
+			return createResponse(HttpStatus.METHOD_NOT_ALLOWED, HttpStatusMessage[HttpStatus.METHOD_NOT_ALLOWED], {
+				message: `Method ${method} not allowed`,
+				method,
+				context: _context,
+			});
+	}
 };
